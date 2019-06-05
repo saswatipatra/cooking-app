eb710e88cb1c878cc88215ca17306308a9bb3b50
Na Hyung Choi and saswati patra <na@>
2019-06-05 14:26:22 -0700

    add conversions from gal to liters and tbsp to grams

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,22 @@ var gallToLiters = function (gallons) {
     return gallons * 3.785;
 };
 
-var inputGallons = parseInt(prompt("Volume in gallons"));
+var tbspToGrams = function (tbsp) {
+    return tbsp * 14.3;
+};
 
-alert(gallToLiters(inputGallons));
+
+$(document).ready(function() {
+	$("form#gallToLiters").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#gallon").val());
+		var result = gallToLiters(number1);
+		$("#output").text(number1 + " gallons = " + result +" liters");
+	});
+	$("form#tbspToGrams").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#tbsp").val());
+		var result = tbspToGrams(number1);
+		$("#output").text(number1 + " tbsp = " + result +" grams");
+	});
+});
